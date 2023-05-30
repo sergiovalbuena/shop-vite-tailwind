8f3ecce2f9cfb959da09f4262e5773ab29c8e560
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-05-30 16:07:01 -0700

    react-router-dom added and routes created

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,25 +1,34 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrders from '../MyOrders'
 import MyOrder from '../MyOrder'
-import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import NotFound from '../NotFound'
+
 import './App.css'
 
-function App() {
+//Router Funtion
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: '/', element: <Home />},
+    {path: '/my-account', element: <MyAccount />},
+    {path: '/my-orders', element: <MyOrders />},
+    {path: '/my-order', element: <MyOrder />},
+    {path: '/sign-in', element: <SignIn />},
+    {path: '/*', element: <NotFound />},
 
+  ])
+  return routes
+}
+
+
+const App = () =>  {
   return (
-    <>
-      <div className="bg-red-100">
-        Hola Mundo 
-        <Home />
-        <MyAccount />
-        <MyOrders />
-        <MyOrder />
-        <NotFound />
-        <SignIn />
-      </div>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
