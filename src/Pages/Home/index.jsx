06aa1d040d6d25b9b92b9de76e0575ab423ade92
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-05-30 19:15:57 -0700

    card component created

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import { Card } from "../../Components/Card";
 import { Layout } from "../../Components/Layout";
 
 export default function Home() {
   return (
     <Layout> 
       Home sweet home
+      <Card />
     </Layout>
   )
 }
