d43f15a1620b37e4dc5b40224610cc041e557884
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-05-30 22:25:30 -0700

    side detail product open and close onClick context

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,19 @@
+import {useContext} from 'react';
+import {ShoppingCartContext} from '../../Context';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 import './styles.css';
 
+
 export const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext)
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail  flex-col fixed right-0 border border-black rounded-lg bg-white`}>
         <div className="flex justify-between items-center p-6">
             <h2 className="font-medium text-xl">Detail</h2>
-            <div>
+            <div
+            className='cursor-pointer'
+            onClick={()=> context.closeProductDetail()}>
               <XMarkIcon className="h-6 w-6 text-gray-500" />
             </div>
 
