ddad8e43b1503c313e05dae163c5fb771dc2b36b
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-05-30 22:47:42 -0700

    styled detail info on the aside div

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,5 +1,5 @@
-import {useContext} from 'react';
-import {ShoppingCartContext} from '../../Context';
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 import './styles.css';
 
@@ -9,16 +9,27 @@ export const ProductDetail = () => {
   console.log('PRORUCT TO SHOW:', context.productToShow)
 
   return (
-    <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail  flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-        <div className="flex justify-between items-center p-6">
-            <h2 className="font-medium text-xl">Detail</h2>
-            <div
-            className='cursor-pointer'
-            onClick={()=> context.closeProductDetail()}>
-              <XMarkIcon className="h-6 w-6 text-gray-500" />
-            </div>
-
+    <aside
+      className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+      <div className='flex justify-between items-center p-6'>
+        <h2 className='font-medium text-xl'>Detail</h2>
+        <div>
+          <XMarkIcon
+            className='h-6 w-6 text-black cursor-pointer'
+            onClick={() => context.closeProductDetail()}></XMarkIcon>
         </div>
+      </div>
+      <figure className='px-6'>
+        <img
+          className='w-full h-full rounded-lg'
+          src={context.productToShow.images[0]}
+          alt={context.productToShow.title} />
+      </figure>
+      <p className='flex flex-col p-6'>
+        <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+        <span className='font-medium text-md'>${context.productToShow.title}</span>
+        <span className='font-light text-sm'>${context.productToShow.description}</span>
+      </p>
     </aside>
   )
 }
