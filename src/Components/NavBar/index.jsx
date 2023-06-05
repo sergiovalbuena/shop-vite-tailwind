7e41d3545fbd1a27a75333fad757ec39c0b7cb65
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-06-04 22:07:38 -0700

    🐣 first step completed

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     <nav className='flex justify-between items-center fixed z-10 top-0  w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
         <li className='font-bold text-lg'>
-          <NavLink to='/'>
+          <NavLink to='/'> 
             Shopi
           </NavLink>
         </li>
